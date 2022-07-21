1c528127e1d78a026cf31432b3987e1ecf1125c9
doantienthongbku <doantienthongbku@gmai.com>
2022-07-21 14:55:45 +0700

    re-organize file

---
+++ core/engines.py
@@ -0,0 +1,113 @@
+import torch
+import os
+import numpy as np
+
+
+def train(opt, tr_dataloader, model, optim, lr_scheduler, val_dataloader=None):
+    '''
+    Train the model with the prototypical learning algorithm
+    '''
+
+    device = 'cuda:0' if torch.cuda.is_available() and opt.cuda else 'cpu'
+
+    if val_dataloader is None:
+        best_state = None
+    train_loss = []
+    train_acc = []
+    val_loss = []
+    val_acc = []
+    best_acc = 0
+
+    best_model_path = os.path.join(opt.experiment_root, 'best_model.pth')
+    last_model_path = os.path.join(opt.experiment_root, 'last_model.pth')
+
+    for epoch in range(opt.epochs):
+        print('=== Epoch: {} ==='.format(epoch))
+        tr_iter = iter(tr_dataloader)
+        model.train()
+        for batch in tqdm(tr_iter):
+            optim.zero_grad()
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            model_output = model(x)
+            loss, acc = loss_fn(model_output, target=y,
+                                n_support=opt.num_support_tr)
+            loss.backward()
+            optim.step()
+            train_loss.append(loss.item())
+            train_acc.append(acc.item())
+        avg_loss = np.mean(train_loss[-opt.iterations:])
+        avg_acc = np.mean(train_acc[-opt.iterations:])
+        print('Avg Train Loss: {}, Avg Train Acc: {}'.format(avg_loss, avg_acc))
+        lr_scheduler.step()
+        if val_dataloader is None:
+            continue
+        val_iter = iter(val_dataloader)
+        model.eval()
+        for batch in val_iter:
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            model_output = model(x)
+            loss, acc = loss_fn(model_output, target=y,
+                                n_support=opt.num_support_val)
+            val_loss.append(loss.item())
+            val_acc.append(acc.item())
+        avg_loss = np.mean(val_loss[-opt.iterations:])
+        avg_acc = np.mean(val_acc[-opt.iterations:])
+        postfix = ' (Best)' if avg_acc >= best_acc else ' (Best: {})'.format(
+            best_acc)
+        print('Avg Val Loss: {}, Avg Val Acc: {}{}'.format(
+            avg_loss, avg_acc, postfix))
+        if avg_acc >= best_acc:
+            torch.save(model.state_dict(), best_model_path)
+            best_acc = avg_acc
+            best_state = model.state_dict()
+
+    torch.save(model.state_dict(), last_model_path)
+
+    for name in ['train_loss', 'train_acc', 'val_loss', 'val_acc']:
+        save_list_to_file(os.path.join(opt.experiment_root,
+                                       name + '.txt'), locals()[name])
+
+    return best_state, best_acc, train_loss, train_acc, val_loss, val_acc
+
+
+def test(opt, test_dataloader, model):
+    '''
+    Test the model trained with the prototypical learning algorithm
+    '''
+    device = 'cuda:0' if torch.cuda.is_available() and opt.cuda else 'cpu'
+    avg_acc = list()
+    for epoch in range(10):
+        test_iter = iter(test_dataloader)
+        for batch in test_iter:
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            model_output = model(x)
+            _, acc = loss_fn(model_output, target=y,
+                             n_support=opt.num_support_val)
+            avg_acc.append(acc.item())
+    avg_acc = np.mean(avg_acc)
+    print('Test Acc: {}'.format(avg_acc))
+
+    return avg_acc
+
+
+def eval(opt):
+    '''
+    Initialize everything and train
+    '''
+    options = get_parser().parse_args()
+
+    if torch.cuda.is_available() and not options.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+    init_seed(options)
+    test_dataloader = init_dataset(options)[-1]
+    model = init_protonet(options)
+    model_path = os.path.join(opt.experiment_root, 'best_model.pth')
+    model.load_state_dict(torch.load(model_path))
+
+    test(opt=options,
+         test_dataloader=test_dataloader,
+         model=model)
---
+++ tools/train.py
@@ -0,0 +1,51 @@
+from tqdm import tqdm
+import numpy as np
+import torch
+import os
+
+from core.parser_util import get_parser
+from init_engine import *
+from core.engines import *
+
+
+def main():
+    '''
+    Initialize everything and train
+    '''
+    options = get_parser().parse_args()
+    if not os.path.exists(options.experiment_root):
+        os.makedirs(options.experiment_root)
+
+    if torch.cuda.is_available() and not options.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+    init_seed(options)
+
+    tr_dataloader = init_dataloader(options, 'train')
+    val_dataloader = init_dataloader(options, 'val')
+    test_dataloader = init_dataloader(options, 'test')
+
+    model = init_protonet(options)
+    optim = init_optim(options, model)
+    lr_scheduler = init_lr_scheduler(options, optim)
+    res = train(opt=options,
+                tr_dataloader=tr_dataloader,
+                val_dataloader=val_dataloader,
+                model=model,
+                optim=optim,
+                lr_scheduler=lr_scheduler)
+    best_state, best_acc, train_loss, train_acc, val_loss, val_acc = res
+    print('Testing with last model..')
+    test(opt=options,
+         test_dataloader=test_dataloader,
+         model=model)
+
+    model.load_state_dict(best_state)
+    print('Testing with best model..')
+    test(opt=options,
+         test_dataloader=test_dataloader,
+         model=model)
+
+
+if __name__ == '__main__':
+    main()
